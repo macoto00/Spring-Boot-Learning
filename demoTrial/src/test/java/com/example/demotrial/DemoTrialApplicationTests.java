f5bf7eaf208ca39fdb3ac983916e58909601a4e4
Tomáš Macoun <117540231+macoto00@users.noreply.github.com>
2023-06-10 21:35:46 +0200

    Add files via upload

---
+++ demoTrial/src/test/java/com/example/demotrial/DemoTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
