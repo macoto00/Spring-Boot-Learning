8dd30327d6767b7b34cbdfd3181286d1d1c01ae4
TomasMacoun <macount@seznam.cz>
2023-06-11 12:13:51 +0200

    Created getAllLinks function in LinkServiceCRUD

--- Trial Demo/Trial Demo 3/trialDemo3/src/main/java/com/example/trialdemo3/services/LinkService.java
+++ Trial Demo/Trial Demo 3/trialDemo3/src/main/java/com/example/trialdemo3/services/LinkService.java
@@ -1,11 +1,12 @@
 package com.example.trialdemo3.services;
 
+import com.example.trialdemo3.DTOS.LinkDTO;
 import com.example.trialdemo3.models.Link;
 
 import java.util.List;
 
 public interface LinkService {
-    List<Link> getAllLinks();
+    List<LinkDTO> getAllLinks();
     void createLink(Link link);
     String getUrlByAlias(String alias);
     void incrementHitCount(String alias);
--- Trial Demo/Trial Demo 3/trialDemo3/src/main/java/com/example/trialdemo3/services/LinkServiceCRUD.java
+++ Trial Demo/Trial Demo 3/trialDemo3/src/main/java/com/example/trialdemo3/services/LinkServiceCRUD.java
@@ -1,10 +1,12 @@
 package com.example.trialdemo3.services;
 
+import com.example.trialdemo3.DTOS.LinkDTO;
 import com.example.trialdemo3.models.Link;
 import com.example.trialdemo3.repositories.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -17,13 +19,28 @@ public class LinkServiceCRUD implements LinkService {
     }
 
     @Override
-    public List<Link> getAllLinks() {
-        return null;
+    public List<LinkDTO> getAllLinks() {
+        List<Link> listOfAllLinks = (List<Link>) linkRepository.findAll();
+        List<LinkDTO> listOfAllLinkDTOs = new ArrayList<>();
+        LinkDTO linkDTO = new LinkDTO();
+        for (Link link : listOfAllLinks) {
+            linkDTO.setId(link.getId());
+            linkDTO.setUrl(link.getUrl());
+            linkDTO.setHitCount(link.getHitCount());
+            linkDTO.setAlias(link.getAlias());
+            listOfAllLinkDTOs.add(linkDTO);
+        }
+        return listOfAllLinkDTOs;
     }
 
     @Override
     public void createLink(Link link) {
-
+        Link newLink = new Link();
+        newLink.setUrl(link.getUrl());
+        newLink.setAlias(link.getAlias());
+        newLink.setHitCount(link.getHitCount());
+        newLink.setSecretCode(generateSecretCode());
+        linkRepository.save(newLink);
     }
 
     @Override
