53c075929470bf5395370208d0cdf634128a062e
TomasMacoun <macount@seznam.cz>
2023-06-11 11:40:03 +0200

    Created folder for project

---
+++ Trial Demo/Trial Demo 3/trialDemo3/src/main/java/com/example/trialdemo3/TrialDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.trialdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrialDemo3Application.class, args);
+    }
+
+}
